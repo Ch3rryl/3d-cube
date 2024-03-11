ce0ecd82ff66a335aee8b2e6a919f2c9fbf3bcdd
ch3rryl <cherryl@live.co.uk>
2024-03-11 13:00:09 +0000

    layout

--- src/Components/ThreeHook.js
+++ src/Components/ThreeHook.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import { useThree } from 'react-three-fiber';
 
-function MyThreeComponent() {
+function ThreeHook() {
   const {
-    gl,
-    scene,
-    camera,
-    raycaster,
-    size,
-    viewport,
-    aspect,
-    mouse,
-    clock,
-    invalidate,
-    intersect,
-    setDefaultCamera,
+    // gl,
+    scene
+    // camera,
+    // raycaster,
+    // size,
+    // viewport,
+    // aspect,
+    // mouse,
+    // clock,
+    // invalidate,
+    // intersect,
+    // setDefaultCamera,
   } = useThree();
 
 
 
   return (
-    <group>
-      {/* Your 3D scene components go here */}
-    </group>
+    <mesh>
+      <boxGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color="#6be092" />
+    </mesh>
   );
 }
 
-export default MyThreeComponent;
+export default ThreeHook;
